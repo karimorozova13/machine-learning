0067e37ba067a7a445ed98b6a34152bd1585edf0
karimorozova13 <k.morozova@tiomarkets.com>
2024-07-04 17:24:21 +0300

    part of titanic data

---
+++ module2/titanik.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import pandas as pd
+
+titanicD = sns.load_dataset('titanic')
+print(titanicD.head())
+
+print(titanicD.groupby(['sex', 'class'], observed=True)[['survived']].mean().unstack())
+
+print(titanicD.pivot_table('survived', index='sex', columns='class'))
+
+age = pd.cut(titanicD['age'], [0, 18, 80])
+print(titanicD.pivot_table('survived', ['sex', age], 'class'))
